b65a7a7a4a92f2dde69991617a8626bd9c2780c4
YRabbit <rabbit@yrabbit.cyou>
2024-02-08 18:34:03 +1000

    wip

--- FemtoRV/RTL/PLL/gen_plls.sh
+++ FemtoRV/RTL/PLL/gen_plls.sh
@@ -16,3 +16,6 @@ echo Generating PLL for ULX3S
 
 echo Generating PLL for ECP5 evaluation board
 ./gen_pll.sh ECP5 12 > pll_ecp5_evn.v
+
+echo Generating PLL for SIPEED Tangnano9k board
+./gen_pll.sh TANGNANO9K 27 > pll_tangnano9k.v
